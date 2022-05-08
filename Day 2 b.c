580ca5df3d573b9443cd261c150848918655cb9c
eliz0214 <105100792+eliz0214@users.noreply.github.com>
2022-05-08 19:39:02 +0530

    Create Day 2 b.c

---
+++ Day 2 b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    int a=7,b=3,c=10,d=9;
+scanf("%d\n", &a);
+    scanf("%d\n", &b);
+    scanf("%d\n", &c);
+    scanf("%d\n", & d);
+    printf("%d",a%b+c%d);
+    
+    
+           return 0;
+           }
+
+
