a9371a4f1c668096049290b960ed3601567989e1
eliz0214 <105100792+eliz0214@users.noreply.github.com>
2022-05-08 16:28:43 +0530

    Create Day 2 a.c

---
+++ Day 2 a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    scanf("%d\n", &a);
+    scanf("%d\n", &b);
+   
+    
+   
+    
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%.2f\n",(float)a/b);
+    return 0;
+    }
+
+
