d6354368c4cbbf29c7415c45cd7523a83dbd3c71
eliz0214 <105100792+eliz0214@users.noreply.github.com>
2022-07-02 17:03:25 +0530

    Create Factorial array.c

---
+++ Factorial array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int fact(int n)
+    {
+    
+    if (n>0){
+        return n*fact(n-1);
+        }
+    else{
+        return 1;
+        }
+    }
+
+int main() {
+    int i,n;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&ar[i]);
+        }
+    for(i=0;i<n;i++){
+        printf("%d\n",fact (ar[i]));
+        }
+               
+    
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
